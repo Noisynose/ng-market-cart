160006abbe29869da953d32655b5039fbc33f801
Alexandre Consigny <consigal@r3d.com>
2018-12-20 15:45:33 -0500

    Implemented recipe selection feature

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
-  styleUrls: ['./recipe-book.component.css']
+  styleUrls: ['./recipe-book.component.css'],
 })
 export class RecipeBookComponent implements OnInit {
+  selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSelectedRecipe(recipe: Recipe): void {
+    this.selectedRecipe = recipe;
   }
-
 }
